e149903ee9c936e773ff326cd3bbd9369d0a30c6
Fiser Frando <fiser.frando@gmail.com>
2021-08-23 22:19:37 -0400

    Item and ItemList resouces added

---
+++ code/api.py
@@ -0,0 +1,36 @@
+from os import name
+from flask import Flask, request
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+items = []
+
+
+class Item(Resource):
+    def get(self, name):
+        item = next(filter(lambda x: x['name']==name, items), None)
+        return {'item': item}, 200 if item else 404
+
+    def post(self, name):
+        if next(filter(lambda x: x['name']==name, items), None):
+            return {'mesage': f'Item {name} already exist, can\'t be created!'}
+            
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'Item list': items}
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+
+if __name__ == "__main__":
+    app.run(port=9897, debug=True) 
